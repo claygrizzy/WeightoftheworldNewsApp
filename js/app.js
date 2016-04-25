1d9d4f912013859252b54fad1f932c876ca4534b
DeemsW <deems.code@gmail.com>
2016-04-25 14:16:35 -0400

    app.js bones in place

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 /* jslint browser: true, esnext: true */
 
 let app = angular.module('WorldNewsApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/main', {
+            controller: 'MainViewController',
+            templateUrl: 'pageviews/main.html',
+        })
+        .when('/interest', {
+            controller: 'InterestViewController',
+            templateUrl: 'pageviews/interest.html',
+        })
+        .when('/saved', {
+            controller: 'SavedViewController',
+            templateUrl: 'pageviews/saved.html',
+        });
+}]);
+
+mainApp.controller('MainViewController', ['$scope', 'NewsService', function ($scope, NewsService) {
+    
+}]);
+
+mainApp.controller('InterestViewController', ['$scope', 'NewsService', function ($scope, NewsService) {
+    
+}]);
+
+mainApp.controller('SavedViewController', ['$scope', 'NewsService', function ($scope, NewsService) {
+    
+}]);
+
+main.factory('NewsService', ['$http', function ($http) {
+    
+}]);
