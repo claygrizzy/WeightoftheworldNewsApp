e51734dda9063d128a2696466348e087ceba193a
Clay Griswold <ccgriswold@gmail.com>
2016-04-27 11:03:43 -0400

    Fixed repeating code issues (hopefully)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /* jslint browser: true, esnext: true */
 
 require('./newsFactory');
+// require('./filters');
 
-let mainApp = angular.module('WorldNewsApp', ['ngRoute', 'NewsService']);
+let mainApp = angular.module('WorldNewsApp', ['ngRoute', 'NewsService', 'FilterService']);
 
 mainApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -27,6 +28,9 @@ mainApp.controller('MainViewController', ['$scope', 'NewsService', function ($sc
         // console.log('the Link call', callLink);
     }
 
+    // $scope.breakingNews.published = $moment("20111031", "YYYYMMDD").fromNow();
+    $scope.breakingNews = NewsService.fetchNews(artPop);
+
     $scope.clickSave = function(selectedArticle){
         // console.log('Article saved');
         NewsService.clickSave(selectedArticle);
@@ -49,64 +53,5 @@ mainApp.controller('InterestViewController', ['$scope', 'NewsService', function 
 mainApp.controller('SavedViewController', ['$scope', 'NewsService', function ($scope, NewsService) {
 
     $scope.savedCollection = NewsService.fetchSaved();
-
-}]);
-
-mainApp.factory('NewsService', ['$http', '$interval', function ($http, $interval) {
-    let news = [];
-    let save = [];
-    let interests = [];
-
-    return{
-        fetchNews: function(newsCall){
-            $http({
-                    method: 'GET',
-                    url: 'http://chat.queencityiron.com/api/news/latest',
-                }).then(function(response){
-                    console.log('response', response.data.stories);
-                    newsCall(response.data.stories);
-                });
-            $interval(function(){
-                $http({
-                    method: 'GET',
-                    url: 'http://chat.queencityiron.com/api/news/latest',
-                }).then(function(response){
-                    console.log('response', response.data.stories);
-                    newsCall(response.data.stories);
-                });
-            },100000);
-        },
-
-        clickSave: function(article){
-            save.push(article);
-            console.log('these saved', save);
-            // angular.copy(article, save);
-        },
-
-        fetchSaved: function() {
-            return save;
-        },
-        addInterest: function (interest) {
-            interest = interest.toLowerCase();
-            console.log(interest);
-            interests.push(interest);
-            console.log(interests);
-        },
-
-        myInterests: function () {
-            console.log(interests);
-            return interests;
-        },
-
-        removeInterest: function (interest) {
-            console.log(interest);
-            function clear(value) {
-                return value !== interest;
-            }
-            angular.copy(interests.filter(clear), interests);
-            console.log(interests);
-        }
-
-    };
 
 }]);
