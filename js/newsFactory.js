a4e6955ad62c80399ed75e1fb53cc487f181dbaa
DeemsW <deems.code@gmail.com>
2016-04-28 15:12:44 -0400

    News array in NewsService populates on page load with AJAX requested news objects. AJAX response ids pulled into separate NewsIDS array. Interval AJAX request compares new ids to NewsIDs array. If new ID isn't in NewsIDs array, news object is pushed to News array and id is pushed to NewsIDs array. Rinse and repeat.

--- js/newsFactory.js
+++ js/newsFactory.js
@@ -1,83 +1,90 @@
 /* jslint browser: true, esnext: true */
+let moment = require('moment');
 
-module.exports = (function (){
-var service = angular.module('NewsService', []);
+module.exports = (function () {
+    var service = angular.module('NewsService', []);
 
-  service.factory('NewsService', ['$http', '$interval', function ($http, $interval) {
-    let news = [];
-    let save = [];
-    let interests = [];
+    service.factory('NewsService', ['$http', '$interval', function ($http, $interval) {
+        let news = [];
+        let newsIDs = [];
+        let save = [];
+        let interests = [];
 
-    return{
-        fetchNews: function(/*newsCall*/){
-          $http({
-              method: 'GET',
-              url: 'http://chat.queencityiron.com/api/news/latest',
-          }).then(function(response){
-              //console.log('response', response.data.stories);
-              angular.copy(response.data.stories, news);
-              console.log('this is the news array', news);
-              //newsCall(response.data.stories);
-          });
-            $interval(function(){
+        return {
+            fetchNews: function () {
                 $http({
                     method: 'GET',
                     url: 'http://chat.queencityiron.com/api/news/latest',
-                }).then(function(response){
-                    console.log('response', response.data.stories);
+                }).then(function (response) {
+                    for (let i = 0; i < response.data.stories.length; i++) {
+                        newsIDs.push(response.data.stories[i].id);
+                    }
                     angular.copy(response.data.stories, news);
-                    console.log('this is the NEW news array', news);
-//                    if (news.length !== response.data.stories.length){
-//                        for (let i = 0; i < response.data.stories.length; i++) {
-//                            if (response.data.stories[i] !== news[i]) {
-//                                news.pop(response.data.stories[i]);
-//                            }
-//                            console.log(news);
-//                        }
-//                    }
-                    //newsCall(response.data.stories);
+                    console.log(news);
+                    console.log(newsIDs);
                 });
-            },10000);
-        },
-        
-        getNews: function () {
-            return news;
-        },
+                $interval(function () {
+                    $http({
+                        method: 'GET',
+                        url: 'http://chat.queencityiron.com/api/news/latest',
+                    }).then(function (response) {
+                        for (let i = 0; i < response.data.stories.length; i++) {
+                            if (newsIDs.includes(response.data.stories[i].id) === false) {
+                                newsIDs.push(response.data.stories[i].id);
+                                news.push(response.data.stories[i]);
+                            }
+                        }
+                        console.log(news);
+                        console.log(newsIDs);
+                    });
+                }, 10000);
+            },
+            
+            getNewsIDs: function () {
+                for (let i = 0; i < news.length; i++) {
+                    newsIDs.push(news.id[i]);
+                }
+                return newsIDs;
+            },
 
-        clickSave: function(article){
-            save.push(article);
-            console.log('these saved', save);
-            // angular.copy(article, save);
-        },
+            getNews: function () {
+                return news;
+            },
 
-        fetchSaved: function() {
-            return save;
-        },
-        addInterest: function (interest) {
-            interests.push(interest);
-            //console.log(interest + ' was added to your interests array');
-            console.log(interests);
-        },
-        
-        flagInterests: function () {
-            console.log('flagged');
-        },
+            clickSave: function (article) {
+                save.push(article);
+                console.log('these saved', save);
+                },
 
-        myInterests: function () {
-            console.log(interests);
-            return interests;
-        },
+            fetchSaved: function () {
+                return save;
+            },
+            
+            addInterest: function (interest) {
+                interests.push(interest);
+                console.log(interests);
+            },
 
-        removeInterest: function (interest) {
-            console.log(interest);
-            function clear(value) {
-                return value !== interest;
+            flagInterests: function () {
+                console.log('flagged');
+            },
+
+            myInterests: function () {
+                console.log(interests);
+                return interests;
+            },
+
+            removeInterest: function (interest) {
+                console.log(interest);
+
+                function clear(value) {
+                    return value !== interest;
+                }
+                angular.copy(interests.filter(clear), interests);
+                console.log(interests);
             }
-            angular.copy(interests.filter(clear), interests);
-            console.log(interests);
-        }
 
-    };
+        };
 
 }]);
 }());
